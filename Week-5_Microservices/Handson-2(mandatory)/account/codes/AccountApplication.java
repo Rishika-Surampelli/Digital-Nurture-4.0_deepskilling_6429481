8ae23dcbfd834514717df5dca2804833d8758112
Rishika-Surampelli <rishikasurampelli@gmail.com>
2025-07-21 13:39:13 +0530

    WEEK-5

--- Week-5_Microservices/Handson-2(mandatory)/account/codes/AccountApplication.java
+++
@@ -1,7 +0,0 @@
-@SpringBootApplication
-@EnableDiscoveryClient
-public class AccountApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(AccountApplication.class, args);
-    }
-}
