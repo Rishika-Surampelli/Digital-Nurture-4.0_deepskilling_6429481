15c00e003e8b393cee626f60afedf5b4628e6649
Rishika-Surampelli <rishikasurampelli@gmail.com>
2025-07-21 13:20:12 +0530

    Week-5

---
+++ Week-4_Spring REST/Handson_5/SecurityProfilesDemo/src/main/java/com/example/demo/SecurityProfilesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProfilesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityProfilesDemoApplication.class, args);
+	}
+
+}
